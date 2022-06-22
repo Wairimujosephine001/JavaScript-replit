f2dbab228224971550a958db77eb5fce0f2e1c2c
wairimujosephine001 <wairimujosephine001@gmail.com>
2022-06-21 23:45:44 -0400

    assignment

---
+++ assets/main.js
@@ -0,0 +1,9 @@
+const welcome = () => {
+    const inputValue = document.querySelector('input').value
+  const generatedElement = document.createElement('p');
+    generatedElement.innerHTML = `<p>Hello ${inputValue}, Welcome to the bootcamp</p>`
+    document.body.appendChild(generatedElement);
+    if(inputValue === '' || inputValue == null){
+    generatedElement.innerHTML = `<p>Please enter your name!!</p>`
+  }
+  }
